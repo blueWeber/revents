23e9b21f9d089738e53b664a7a24e8504dbe8341
blueWeber <blueweber@gmail.com>
2022-06-20 04:13:23 +0900

    End of Section 3

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
+import './styles.css';
+
+function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
+  return (
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
