83507bf59eb5c0ba421d0068c70926e719f685be
blueWeber <blueweber@gmail.com>
2022-06-22 00:02:51 +0900

    End of section 4

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,28 +1,104 @@
-import React from 'react';
+import cuid from 'cuid';
+import React, { useState } from 'react';
 import { Segment, Header, Form, Button } from 'semantic-ui-react';
 
-const EventForm = ({ setFormOpen }) => {
+const EventForm = ({
+  setFormOpen,
+  setEvents,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}) => {
+  const initialValues = selectedEvent ?? {
+    title: '',
+    category: '',
+    description: '',
+    city: '',
+    venue: '',
+    date: '',
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...values })
+      : createEvent({
+          ...values,
+          id: cuid(),
+          hostedBy: 'Bob',
+          attendees: [],
+          hostPhotoURL: '/assets/user.png',
+        });
+    setFormOpen(false);
+  };
+
+  const handleInputChange = (e) => {
+    e.persist();
+    const { name, value } = e.target;
+    const newValues = { ...values, [name]: value };
+    setValues(newValues);
+  };
+
   return (
     <Segment clearing>
-      <Header content='Create new event' />
-      <Form>
+      <Header content={selectedEvent ? 'Edit the event' : 'Create new event'} />
+      <Form onSubmit={handleFormSubmit}>
         <Form.Field>
-          <input type='text' placeholder='Event Title' />
+          <input
+            type='text'
+            placeholder='Event Title'
+            value={values.title}
+            onChange={handleInputChange}
+            name='title'
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Category' />
+          <input
+            type='text'
+            placeholder='Category'
+            value={values.category}
+            onChange={handleInputChange}
+            name='category'
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Description' />
+          <input
+            type='text'
+            placeholder='Description'
+            value={values.description}
+            onChange={handleInputChange}
+            name='description'
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='City' />
+          <input
+            type='text'
+            placeholder='City'
+            value={values.city}
+            onChange={handleInputChange}
+            name='city'
+          />
         </Form.Field>
         <Form.Field>
-          <input type='text' placeholder='Venue' />
+          <input
+            type='text'
+            placeholder='Venue'
+            value={values.venue}
+            onChange={handleInputChange}
+            name='venue'
+          />
         </Form.Field>
         <Form.Field>
-          <input type='date' placeholder='Date' />
+          <input
+            type='date'
+            placeholder='Date'
+            value={values.date}
+            onChange={handleInputChange}
+            name='date'
+          />
         </Form.Field>
         <Button type='submit' floated='right' positive content='Submit' />
         <Button
